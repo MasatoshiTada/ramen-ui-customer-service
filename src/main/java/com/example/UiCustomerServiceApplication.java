537e1cacaf538bee77a9eae18bdae9e60d1a208a
多田真敏 <tada@tadamasatoshinoiMac.local>
2017-07-28 16:42:00 +0900

    Configクラスを修正

--- src/main/java/com/example/UiCustomerServiceApplication.java
+++ src/main/java/com/example/UiCustomerServiceApplication.java
@@ -13,13 +13,13 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -61,7 +61,7 @@ public class UiCustomerServiceApplication {
         return new RequestDumperFilter();
     }
 
-    @EnableWebSecurity
+    @Configuration
     static class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
